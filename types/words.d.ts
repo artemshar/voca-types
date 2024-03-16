40cc5f5d84cbf1315dda5b6998bb6ab2eaf314d9
artemshar <artem.ude@gmail.com>
2024-03-16 22:14:27 +0100

    added types: words, layout

---
+++ types/words.d.ts
@@ -0,0 +1,31 @@
+export interface IWord {
+  id: string;
+  word: string;
+  color?: string;
+  dateAdded: {
+    date: string;
+    time: string;
+    full: Date | string;
+  };
+  ownDescription: string;
+  imageURL: string;
+  language: ILanguage; // model Language
+	includedInLists?: IList[] // lists to which the word has been added
+}
+export type ILanguage = {
+	id: string,
+	label: string,
+}
+export interface IList {
+  id: string;
+	name: string;
+	description?: string;
+	coverURL?: string;
+	dateAdded: {
+    date: string;
+    time: string;
+    full: Date | string;
+  };
+	url: string;
+	language: ILanguage;
+}
